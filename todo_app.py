84b94d3f7ecaf7c7b825eff6bb1f8e96ff6ab313
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-09-28 12:01:20 +0200

    feat(todo_app): add command list, printer, tested

--- todo_app.py
+++ todo_app.py
@@ -10,3 +10,18 @@ class TodoApp(object):
             {'arg': '-r', 'desc': 'Removes a task'},
             {'arg': '-c', 'desc': 'Completes a task'}
         ]
+    def print_commands(self):
+        print('Command Line Todo application\n' + 
+              '==============================\n\n' + 
+              'Command line arguments:')
+        for i in self.commands:
+            print(i['arg'] + '  ' + i['desc'])
+
+
+def start_app():
+    app = TodoApp()
+    args = sys.argv[1:]
+    if not args:
+        app.print_commands()
+
+start_app()
